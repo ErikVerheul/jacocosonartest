09cc0c39ab230dc14f251692895d664c1f8ea30f
Erik Verheul <erik@verheulconsultants.nl>
2018-05-14 18:18:56 +0200

    Coverage report ok when sources from modules are duplicated in integration module.

---
+++ integrationtests/src/main/java/mod1package/OnlyRegressionTested.java
@@ -0,0 +1,9 @@
+package mod1package;
+
+public class OnlyRegressionTested {
+  private final int a = 6;
+  public int doSomething() {
+    System.out.println("Mod1-OnlyRegressionTested:doSomething returns: " + a);
+    return a;
+  }
+}
